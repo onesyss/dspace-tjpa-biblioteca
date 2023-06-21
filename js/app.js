721964ca967bec12d253b85ac76d50efb019351b
Marlon Miranda <47862809+onesyss@users.noreply.github.com>
2023-06-21 10:46:01 -0300

    first commit

--- js/app.js
+++ js/app.js
@@ -7,3 +7,43 @@ anoFooter.innerText = year.getFullYear();
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+// const collpases = document.querySelectorAll('.collapse')
+
+// for (const coll of collpases) {
+//   coll.addEventListener('click', () => {
+//     console.log(coll.classList)
+//   })
+// }
+
+const leads = document.querySelectorAll('.lead')
+const collapses = document.querySelectorAll('.card-collapse .collapse')
+
+
+console.log(collapses)
+
+
+
+
+for (const i in leads) {
+  const lead = leads[i]
+
+
+
+
+  lead.onclick = () => {
+
+
+    for (let a = 0; a < 7; a++) {
+      if (collapses[a] !== collapses[i]) {
+        collapses[a].classList.remove('show')
+
+      }
+
+
+    }
+
+
+
+  }
+}
