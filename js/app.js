33b3166cf3461e9ba29fc038b0a3515a6558d44b
Marlon Miranda <47862809+onesyss@users.noreply.github.com>
2023-06-26 13:47:15 -0300

    centralizacao correta dos elementos

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
-
-let anoFooter = document.getElementById('ano-atual');
+let anoFooter = document.getElementById("ano-atual");
 
 year = new Date();
 anoFooter.innerText = year.getFullYear();
 
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
 // const collpases = document.querySelectorAll('.collapse')
 
@@ -16,34 +15,18 @@ $(function () {
 //   })
 // }
 
-const leads = document.querySelectorAll('.lead')
-const collapses = document.querySelectorAll('.card-collapse .collapse')
+const leads = document.querySelectorAll(".lead");
+const collapses = document.querySelectorAll(".card-collapse .collapse");
 
-
-console.log(collapses)
-
-
-
+console.log(collapses);
 
 for (const i in leads) {
-  const lead = leads[i]
-
-
-
-
+  const lead = leads[i];
   lead.onclick = () => {
-
-
     for (let a = 0; a < 7; a++) {
       if (collapses[a] !== collapses[i]) {
-        collapses[a].classList.remove('show')
-
+        collapses[a].classList.remove("show");
       }
-
-
     }
-
-
-
-  }
+  };
 }
